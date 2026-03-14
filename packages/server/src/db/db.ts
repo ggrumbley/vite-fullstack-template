e2419c43ea936d06ca4ceb30f8e81bafde94db57
Gary Grumbley <ggrumbley@classy.org>
2026-03-14 14:21:48 -0700

    feat: add seeding

--- packages/server/src/db/db.ts
+++ packages/server/src/db/db.ts
@@ -1,7 +1,11 @@
+import { drizzle } from 'drizzle-orm/node-postgres'; // For local
+import { drizzle as neonDrizzle } from 'drizzle-orm/neon-http'; // For production
+import { neon } from '@neondatabase/serverless';
+import pg from 'pg';
 import 'dotenv/config';
 
-import { drizzle } from 'drizzle-orm/neon-http';
-import { neon } from '@neondatabase/serverless';
+const connectionString = process.env.DATABASE_URL!;
 
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle({ client: sql });
+export const db = connectionString.includes('neon.tech')
+  ? neonDrizzle(neon(connectionString))
+  : drizzle(new pg.Pool({ connectionString }));
